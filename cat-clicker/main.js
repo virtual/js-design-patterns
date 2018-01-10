4e1aa1876ee5d244b9713f39e9d52e8567c015ab
Jeanine Schoessler <jeanine.mt@gmail.com>
2018-01-09 17:44:20 -0700

    add photos and create thumbnail feature module

--- cat-clicker/main.js
+++ cat-clicker/main.js
@@ -1,38 +1,76 @@
 let animals = {
   "cats": [{
-      "image": "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
-      "name": "Whiskers"
+      "image": "images/alexandru-zdrobau-176844.jpg",
+      "name": "Whiskers",
+      "count": 0
     },
     {
-      "image": "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&amp;h=426",
-      "name": "Pillow"
+      "image": "images/cat-mapper-max-ogden-204436.jpg",
+      "name": "Cassandra",
+      "count": 0
+    },
+    {
+      "image": "images/eric-han-455080.jpg",
+      "name": "Trix",
+      "count": 0
+    },
+    {
+      "image": "images/nirzar-pangarkar-217994.jpg",
+      "name": "Lila",
+      "count": 0
+    },
+    {
+      "image": "images/pacto-visual-157776.jpg",
+      "name": "Phoenix",
+      "count": 0
+    },
+    {
+      "image": "images/paul-291714.jpg",
+      "name": "Clawed",
+      "count": 0
     }
   ]
 }
-function displayAnimals(obj) {
+let prev;
+
+function displayGridItem(e, i) {
+  let id = (i !== undefined) ? "photo"+i : "thefeature";
+ console.log(e) 
+ return '<figure id="'+id + '"> \
+ <figcaption>' + e.name + ': <span class="clickcount">' + e.count + '</span> clicks</figcaption> \
+ <img src=' + e.image + ' alt="' + e.name + '" /> \
+</figure>';
+}
+function displayGrid(obj) {
   var html = '';
   obj.map((e, i) => {
-    html += '<figure id="photo' + i + '"> \
-      <figcaption>' + e.name + ': <span class="clickcount">0</span> clicks</figcaption> \
-      <img src=' + e.image + ' alt="' + e.name + '" /> \
-    </figure>';
+    html += displayGridItem(e, i);
+    
   });
   return html;
 }
 function handleClick(e) {
   var span = document.getElementById(e).childNodes[1].getElementsByTagName('span')[0];
-  span.innerHTML = parseInt(span.innerHTML) + 1;
+  if (prev) document.getElementById(prev).className = ""
+  document.getElementById(e).className = "active"
+  let count = parseInt(span.innerHTML) + 1;
+  span.innerHTML = count;
+  prev = e;
 }
 function displayCats() {
-  document.getElementById("animals").innerHTML = displayAnimals(animals.cats);
+  document.getElementById("animals").innerHTML = displayGrid(animals.cats);
   var photos = Array.prototype.slice.call(document.getElementsByTagName('figure'));
   photos.forEach((e, i) => {
     e.addEventListener('click', (function (index) {
       return function () {
         handleClick("photo" + index);
+        animals.cats[index].count = parseInt(animals.cats[index].count) + 1;
+        document.getElementById("feature").innerHTML = displayGridItem(animals.cats[index])
+        console.log(animals.cats[index].count)
       };
     })(i));
   });
 }
 
 displayCats();
+document.getElementById("feature").innerHTML = displayGridItem(animals.cats[0])
